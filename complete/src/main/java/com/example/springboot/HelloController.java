435f2e87918cfad813b00c1ebafe9254fdd60114
ist192510 <ist192510@tecnico.ulisboa.pt>
2022-01-06 17:34:49 +0000

    starting endpoints

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
+	@RequestMapping(value="/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
+
+	@RequestMapping(value="/Login")
+	public static String Welcome() {
+		return "Mobile \n" +
+				"Login Client \n" +
+				"Login Costumer";
+	}
+
+
 
 }
