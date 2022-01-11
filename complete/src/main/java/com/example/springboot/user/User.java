2f969c05b79f006e2ec5babf5e9e5ab15486eb8c
sanchabbarroso <spbbbarroso@gmail.com>
2022-01-11 15:42:34 +0000

    added smartphone app basic structure with attempt of REST connection

--- complete/src/main/java/com/example/springboot/user/User.java
+++ complete/src/main/java/com/example/springboot/user/User.java
@@ -3,9 +3,26 @@ package com.example.springboot.user;
 public class User {
 
     private int userId;
+    private String username;
+    private String password;
 
-    public User(int id) {
+    public User(int id, String name, String pass) {
         this.userId = id;
+        this.username = name;
+        this.password = pass;
+    }
+
+    public int getId() {
+        return this.userId;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
     }
 
 }
+
