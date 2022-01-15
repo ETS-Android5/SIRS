851bf5cb8887dc3aedcf68a9f08e73216c5ea86f
sanchabbarroso <spbbbarroso@gmail.com>
2022-01-15 17:20:53 +0000

    Implementation of TLS, for testing

--- complete/src/main/java/com/example/springboot/Application.java
+++ complete/src/main/java/com/example/springboot/Application.java
@@ -3,8 +3,16 @@ package com.example.springboot;
 import com.example.springboot.helpers.DBHelper;
 import com.example.springboot.helpers.KeyGenerator;
 import com.example.springboot.user.User;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,6 +29,37 @@ public class Application {
 		//DBHelper.createMobileTable();
 		SpringApplication.run(Application.class, args);
 		//DBHelper.insertUser(1, "user1", "pass1");
+
+	}
+
+	@Configuration
+	public class ServerConfig {
+
+		@Bean
+		public ServletWebServerFactory servletContainer() {
+			TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+				@Override
+				protected void postProcessContext(Context context) {
+					SecurityConstraint securityConstraint = new SecurityConstraint();
+					securityConstraint.setUserConstraint("CONFIDENTIAL");
+					SecurityCollection collection = new SecurityCollection();
+					collection.addPattern("/*");
+					securityConstraint.addCollection(collection);
+					context.addConstraint(securityConstraint);
+				}
+			};
+			tomcat.addAdditionalTomcatConnectors(getHttpConnector());
+			return tomcat;
+		}
+
+		private Connector getHttpConnector() {
+			Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+			connector.setScheme("http");
+			connector.setPort(8080);
+			connector.setSecure(false);
+			connector.setRedirectPort(8443);
+			return connector;
+		}
 
 	}
 
