77e3534ea765593607b9ae96415bfd584d9af487
O Seu Nome <utilizador@exemplo.com>
2022-01-27 00:19:11 +0000

    Remover cócó

--- SmartphoneApplication/app/src/main/java/com/example/smartphoneapplication/totp/TOTPException.java
+++
@@ -1,9 +0,0 @@
-package com.example.smartphoneapplication.totp;
-
-import org.jetbrains.annotations.NotNull;
-
-public final class TOTPException extends Exception {
-    public TOTPException(@NotNull String message, @NotNull Exception cause) {
-        super(message, (Throwable)cause);
-    }
-}
